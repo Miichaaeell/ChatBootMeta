4ab66bf2e3915d8d79de1326958a1abf63eec902
Michael Andrew <96355111+Miichaaeell@users.noreply.github.com>
2024-05-24 17:42:31 -0300

    upload files

---
+++ funcoesbd.py
@@ -0,0 +1,93 @@
+import mysql.connector
+import os
+import smtplib
+import email.message
+
+key_email = os.getenv("EMAIL_KEY")
+my_email = os.getenv("MY_EMAIL")
+key_bd = os.getenv("BD_KEY")
+
+
+def cadastro(nome, telefone):
+    conexao = mysql.connector.connect(
+        host='monorail.proxy.rlwy.net',
+        port='34425',
+        user='root',
+        password=f'{key_bd}',
+        database='railway',
+    )
+    cursor = conexao.cursor()
+    seleciona = f'SELECT Nome, Telefone FROM clientes'
+    cursor.execute(seleciona)
+    dados = cursor.fetchall()
+    cadastrado = False
+    for cliente, numero in dados:
+        if int(numero) == int(telefone):
+            cadastrado = True
+    if cadastrado == False:
+        nome = nome.split()
+        cadastrando = f'INSERT INTO `railway`.`clientes` (`Nome`, `Telefone`) VALUES ("{nome[0]}", "{telefone}")'
+        cursor.execute(cadastrando)
+        conexao.commit()
+        return str('falso')
+    else:
+        return str('verdadeiro')
+    cursor.close()
+    conexao.close()
+
+
+def cadastrar_email(email, telefone):
+    conexao = mysql.connector.connect(
+        host='monorail.proxy.rlwy.net',
+        port='34425',
+        user='root',
+        password=f'{key_bd}',
+        database='railway',
+    )
+
+    cursor = conexao.cursor()
+    comando = f"UPDATE `railway`.`clientes` SET `Email` = '{email[6:].strip()}' WHERE (`Telefone` = '{telefone}')"
+    cursor.execute(comando)
+    conexao.commit()
+    conexao.close()
+    cursor.close()
+
+
+def consultar_email(telefone):
+    conexao = mysql.connector.connect(
+        host='monorail.proxy.rlwy.net',
+        port='34425',
+        user='root',
+        password=f'{key_bd}',
+        database='railway',
+    )
+    cursor = conexao.cursor()
+    comando = f'SELECT Email, Telefone FROM clientes WHERE Telefone = {telefone}'
+    cursor.execute(comando)
+    contato = cursor.fetchall()
+    for e_mail, telefone in contato:
+        email = e_mail
+    return str(email)
+    cursor.close()
+    conexao.close()
+
+
+def enviar_email(nome='', telefone='', msg=''):
+    adress = [consultar_email(telefone), my_email]
+    corpo_email = f"""Olá {nome}, seu pedido de orçamento foi realizado com Sucesso!
+    \nDetalhes da solicitação:\n--> {msg}\n\nAtt.\nMichael Dev
+    """
+    for endereco in adress:
+        msg = email.message.Message()
+        msg['Subject'] = 'Confirmação do pedido de orçamento - no reply'
+        msg['From'] = f'{my_email}'
+        msg['To'] = endereco
+        password = f'{key_email}'
+        msg.add_header('Content-Type', 'text')
+        msg.set_payload(corpo_email)
+        s = smtplib.SMTP('smtp.gmail.com:587')
+        s.starttls()
+        s.login(msg['From'], password)
+        s.sendmail(msg['From'], msg['To'], msg.as_string().encode('utf-8'))
+        print(msg['To'])
+        print('Email enviado com sucesso!')
---
+++ main.py
@@ -0,0 +1,49 @@
+import requests
+import json
+from flask import Flask, request
+import os
+from gerencia import filtar_dados, cadastrar_usuario, reply
+from respostas import resposta_formatada
+
+token = os.getenv('TOKEN')
+Bearer_TOKEN = os.getenv('Bearer_TOKEN')
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return f'Home Page'
+
+
+@app.get('/webhook')
+def verify_key():
+    key = request.args.get('hub.challenge')
+    verify_token = request.args.get('hub.verify_token')
+    if verify_token == token:
+        return key
+    else:
+        print('Token incorreto')
+
+
+@app.post('/webhook')
+def bot():
+    msg = request.get_json()
+    if 'contacts' in msg['entry'][0]['changes'][0]['value']:
+        msg_usuario = filtar_dados(msg)
+        verificar_cadastro = cadastrar_usuario(msg_usuario)
+        res = reply(verificar_cadastro[0], verificar_cadastro[1])
+        print(res)
+        url = f'https://graph.facebook.com/v19.0/254050617801879/messages'
+        data = resposta_formatada(res)
+        json.dumps(data)
+        r = requests.request(method='POST',
+                             url=url,
+                             headers={'Authorization': f'Bearer {Bearer_TOKEN}'},
+                             json=data)
+        return str(f'{r}')
+    else:
+        return 'Notificações'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
